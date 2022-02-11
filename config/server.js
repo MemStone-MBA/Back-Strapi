3e219cc51e8578e681a86ab01195c6ee640f8289
root <root@vps-722995c4.vps.ovh.net>
2022-02-11 20:10:14 +0000

    reword node 14

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd540f83542cc7a905812abb596e6b1de'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f90cf91d-aecb-4aa1-9785-b5b7c4eea6fa'
+};
