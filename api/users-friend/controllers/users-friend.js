f3ba26ad0347070a174b5ed5d693713d7398897f
kirtsu04 <lucas.ru74@gmail.com>
2022-05-02 15:30:01 +0200

    add route findUserFriendsByUsername

--- api/users-friend/controllers/users-friend.js
+++ api/users-friend/controllers/users-friend.js
@@ -16,7 +16,7 @@ module.exports = {
         all.push(elem)
       })
 
-      
+
       // use map() to perform a fetch and handle the response for each url
       let result = await Promise.all(all.map(  (userFriend)  =>  {
 
@@ -26,6 +26,39 @@ module.exports = {
         }else(userFriend.userFriendId2 == id)
         {
           return strapi.db.query('user', 'users-permissions').findOne({"id": userFriend.userFriendId1});
+        }
+      }))
+      .then(data => {
+        return  data
+      }).catch(err => {
+        return {status:"err"}
+      })
+
+      return result;
+    } catch (err) {
+      return err;
+    }
+    finally {
+    }
+  },
+  findUserFriendsByUsername: async (ctx) => {
+    try {
+
+      const username = ctx.params.username || "0000000"
+
+      let data = await strapi.services['users-friend'].find( {"username" :username})
+
+      let all = [];
+      data?.forEach(elem=>{
+        all.push(elem)
+      })
+
+      // use map() to perform a fetch and handle the response for each url
+      let result = await Promise.all(all.map(  (userFriend)  =>  {
+
+        if(userFriend.username == username)
+        {
+          return strapi.db.query('user', 'users-permissions').findOne({"username": userFriend.username});
         }
       }))
       .then(data => {
